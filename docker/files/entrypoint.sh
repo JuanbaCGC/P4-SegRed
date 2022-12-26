5c99cf8273057eee833db4e2e9195fc41f843741
JuanbaCGC <JuanBautista.Castano@alu.uclm.es>
2022-12-26 08:15:18 -0500

    new network and containers

---
+++ docker/files/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p icmp -j ACCEPT
+
+ip route del default
+ip route add default via 10.0.2.2 dev eth0 
+
+service ssh start
+service rsyslog start
+
+if [ -z "$@" ]; then
+    exec /bin/bash
+else
+    exec $@
+fi
