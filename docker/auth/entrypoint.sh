34dbacd525598bcf1dd2367293d1827f8003afbb
JuanbaCGC <JuanBautista.Castano@alu.uclm.es>
2022-12-28 11:56:27 -0500

    new iptables

--- docker/auth/entrypoint.sh
+++ docker/auth/entrypoint.sh
@@ -13,11 +13,12 @@ ip route add default via 10.0.2.2 dev eth0
 iptables -A INPUT -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
 
 # Aceptar TCP que venga del broker y del files
-# iptables -A INPUT -s 10.0.1.4 -p tcp --dport 5000 -j ACCEPT
-# iptables -A INPUT -s 10.0.2.4 -p tcp --dport 5000 -j ACCEPT
-# iptables -A INPUT -s 10.0.1.2 -p tcp --dport 5000 -j ACCEPT
-iptables -A INPUT -p tcp -s 10.0.1.0/24 -j ACCEPT
-iptables -A INPUT -p tcp -s 10.0.2.2 -j ACCEPT
+iptables -A INPUT -s 10.0.1.4 -p tcp --dport 5000 -j ACCEPT
+iptables -A INPUT -s 10.0.2.4 -p tcp --dport 5000 -j ACCEPT
+iptables -A INPUT -s 10.0.1.2 -p tcp --dport 5000 -j ACCEPT
+
+#iptables -A INPUT -p tcp -s 10.0.1.0/24 -j ACCEPT
+
 
 service ssh start
 service rsyslog start
